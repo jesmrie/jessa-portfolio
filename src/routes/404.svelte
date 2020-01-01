0f5da17f6a38437eee726fa03bedbb8954acff22
Jessa Pedrola <jessa@symph.co>
2020-01-02 03:21:33 +0800

    404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,24 @@
+<style>
+  a {
+    color: #77c9ff;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+</style>
+
+
+<svelte:head>
+  <title>Page Not Found</title>
+</svelte:head>
+
+<div class="container text-center mt-5">
+  <img
+    src="images/404.svg"
+    class="mb-5 w-100 undraw-illus"
+    alt="Not found by Katerina Limpitsouni of unDraw" />
+  <h2>Page not found</h2>
+  <p>The page you are looking for doesn’t exist.</p>
+  <a href="/">Go back to about</a>
+</div>
